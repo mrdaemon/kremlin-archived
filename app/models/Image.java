ab5d4328349d33d25c7825c926f5e7aac4868aed
Alexandre Gauthier <alex@lab.underwares.org>
2011-02-07 20:01:36 -0500

    Add validation to Image model, remove filename generation from callbacks
    Image gets a UUID in the Blob class on filesystem as a filename,
    so storing that completely defeats the purpose.

--- app/models/Image.java
+++ app/models/Image.java
@@ -7,6 +7,7 @@ import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import play.data.validation.Required;
 import play.db.jpa.Blob;
 import play.db.jpa.Model;
 
@@ -15,22 +16,27 @@ import utils.Checksum;
 @Entity
 @Table(uniqueConstraints=@UniqueConstraint(columnNames="file_sha1"))
 public class Image extends Model {
+	@Required
 	public String filename;
 	
+	@Required
 	@Column(name="file_sha1", unique=true, nullable=false)
 	public String checksum;
 	
+	@Required
 	public Blob imagefile;
+	
+	//public Blob thumbnailfile;
 	
 	@PrePersist
 	protected void onCreate(){
-		filename = imagefile.getFile().getName();
 		checksum = Checksum.generateSHA1Checksum(imagefile.getFile());
 	}
 	
 	@PreUpdate
 	protected void onUpdate(){
-		filename = imagefile.getFile().getName();
 		checksum = Checksum.generateSHA1Checksum(imagefile.getFile());
 	}
+	
+	
 }
